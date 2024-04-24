806b2b68ac8ae532288775aa4e78cc96c8354260
NarendraReddy <narendra.nya@gmail.com>
2024-04-24 13:46:36 -0700

    disk

--- 03-Diskusagepr.sh
+++ 03-Diskusagepr.sh
@@ -5,7 +5,7 @@ THRESHOLD=6
 
 while IFS= read -r line 
 do 
-    USAGE=$(echo $line | grep xfs|awk -F " " '{print $6F}')
+    USAGE=$(echo $line | grep xfs|awk -F " " '{print $6F}' | cut -d "%" -f1)
     echo $USAGE
     
 done <<<$DISK_USAGE
