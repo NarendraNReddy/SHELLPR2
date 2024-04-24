244ea4c69423d7e1ec988cf1fdb8881e27f83063
NarendraReddy <narendra.nya@gmail.com>
2024-04-24 13:29:17 -0700

    disk

--- 02-Diskusage.sh
+++ 02-Diskusage.sh
@@ -6,6 +6,7 @@ THRESHOLD=6
 
 while IFS= read -r line 
 do 
-    USAGE=$(echo $line | awk -F " " '{print $6F}')
+    USAGE=$(echo $line | awk -F " " '{print $6F}'| cut -d "%" -f1)
     echo $USAGE
+    
 done <<<$DISK_USAGE
