ba53a46708a0f7c9663296e87757d27042e69631
NarendraReddy <narendra.nya@gmail.com>
2024-04-24 13:49:27 -0700

    disk

--- 03-Diskusagepr.sh
+++ 03-Diskusagepr.sh
@@ -7,6 +7,10 @@ while IFS= read -r line
 do 
     USAGE=$(echo $line | grep xfs|awk -F " " '{print $6F}' | cut -d "%" -f1)
     FOLDER=$(echo $line | grep xfs | awk -F " " '{print $NF}')
-    echo $FOLDER
+    
+    if [ $USAGE -ge $THRESHOLD ];
+    then
+        echo "$FOLDER exceeds the $THRESHOLD,current usage :$USAGE" 
+    fi 
     
 done <<<$DISK_USAGE
